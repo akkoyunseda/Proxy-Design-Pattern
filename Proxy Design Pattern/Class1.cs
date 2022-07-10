58df8e62f0eaa62786ba9c1057f8a7b7df5f3fcd
Sedanur Akkoyun <akkoyunsedanur@gmail.com>
2022-07-10 16:13:25 +0300

    Smart Home Information System with Proxy
     A simple smart home information system application for understand how Proxy works.

---
+++ Proxy Design Pattern/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proxy1
+{   
+    /// <summary>
+    /// Real concrete ve proxy sınıflarının kalıtım alacağı interface
+    /// </summary>
+    public interface ISunucu
+    {
+        void SiparişAl(string sipariş);
+        string SiparişTeslim();
+        void OdemeAl(string odeme);
+    }
+
+
+    /// <summary>
+    /// Real concrate sınıfı
+    /// </summary>
+    class Servis : ISunucu
+    {
+        private string Sipariş;
+        public void SiparişAl(string sipariş)
+        {
+            Console.WriteLine("Sunucu " + sipariş + " için sipariş oluşturuyor .\n");
+            Sipariş = sipariş;
+        }
+        
+        public string SiparişTeslim()
+        {
+            return Sipariş;
+        }
+
+        public void OdemeAl(string odeme)
+        {
+            Console.WriteLine("Sipariş için ödeme (" + odeme + ") oluşturuldu.");
+        }
+    }
+    
+    /// <summary>
+    /// Proxy sınıfı
+    /// </summary>
+    class Proxy : ISunucu
+    {
+        private string Sipariş;
+        private Servis _servis = new Servis();
+
+        public void SiparişAl(string sipariş)
+        {
+            Console.WriteLine("(Sunucu " + sipariş + " için sipariş alıyor...)");
+            Sipariş = sipariş;
+            System.Threading.Thread.Sleep(2000);
+        }
+
+        public string SiparişTeslim()
+        {
+            return Sipariş;
+        }
+
+        public void OdemeAl(string odeme)
+        {
+            Console.WriteLine("(Sunucu ödemeyi işliyor lütfen bekleyin!)\n");
+            System.Threading.Thread.Sleep(2000);
+            _servis.OdemeAl(odeme);
+        }
+    }
+}
---
+++ Proxy Design Pattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proxy1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proxy proxy = new Proxy();
+
+            Console.WriteLine("Ne sipariş etmek istersiniz?");
+            string sipariş = Console.ReadLine();
+            proxy.SiparişAl(sipariş);
+            Console.WriteLine("Tabii! Buyurun " + proxy.SiparişTeslim() + " . \n");
+            Console.WriteLine("Nasıl ödemek istersiniz?");
+            string ödeme = Console.ReadLine();
+            proxy.OdemeAl(ödeme);
+
+            Console.ReadKey();
+
+        }
+    }
+}
